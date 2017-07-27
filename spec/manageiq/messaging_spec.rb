ae9de0fd5439f9661b562320716bc7ab8491ef92
Oleg Barenboim <chessbyte@gmail.com>
2017-07-27 08:01:50 -0400

    Correct "Manageiq" to "ManageIQ" in spec

--- spec/manageiq/messaging_spec.rb
+++ spec/manageiq/messaging_spec.rb
@@ -1,8 +1,8 @@
 require "spec_helper"
 
-describe Manageiq::Messaging do
+describe ManageIQ::Messaging do
   it "has a version number" do
-    expect(Manageiq::Messaging::VERSION).not_to be nil
+    expect(ManageIQ::Messaging::VERSION).not_to be nil
   end
 
   it "does something useful" do
