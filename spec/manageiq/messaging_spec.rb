e7dc5ff555a05212fb01b34775a68085a3e95008
Oleg Barenboim <chessbyte@gmail.com>
2017-07-27 08:12:30 -0400

    Removed auto-generated test that fails on purpose

--- spec/manageiq/messaging_spec.rb
+++ spec/manageiq/messaging_spec.rb
@@ -4,8 +4,4 @@ describe ManageIQ::Messaging do
   it "has a version number" do
     expect(ManageIQ::Messaging::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
